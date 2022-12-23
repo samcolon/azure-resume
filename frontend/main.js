e644778a605b5c288f2b4938ff984e842f3fbbe8
samcolon <116681627+samcolon@users.noreply.github.com>
2022-12-23 06:00:20 -0500

    update to main.js

--- frontend/main.js
+++ frontend/main.js
@@ -3,19 +3,19 @@ window.addEventListener('DOMContentLoaded', (event) =>{
 });
 
 
-const functionApi = 'http://localhost:7071/api/GetVisitorCount';
-/*const functionApiUrl = 'http://localhost:7071/api/GetVisitorCount';*/
+const localFunctionApi = 'http://localhost:7071/api/GetVisitorCount';
+const functionApiUrl = 'https://azuregetvisitorcounter.azurewebsites.net/api/GetVisitorCount?code=1Ji4jGb14lXv95jtVoGPawvDnzb40aSs5tNc-cOOm2haAzFuQv1gtg==';
 
 const getVisitCount = () => {
     let count = 30;
-    fetch(functionApi).then(response => {
+    fetch(localFunctionApi).then(response => {
         return response.json()
     }).then(response => {
-        console.log("Website called function API");
-        count = response.count
+        console.log("Website called function API",response);
+        count = response.Count;
         document.getElementById("counter").innerText = count;
     }).catch(function(error) {
-        console.log(error);
+        console.log("error",error);
     });
     return count;
 }
